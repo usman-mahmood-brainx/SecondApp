26447575d903779214db93d059161338acb5518a
usman.mahmood@brainxtech.com <bMGF!c3n>
2023-03-22 17:54:16 +0500

    Change 4 commit

--- app/src/main/java/com/example/secondapp/MainActivity.kt
+++ app/src/main/java/com/example/secondapp/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
         var a = 2;
-        println("Change 2");
+        println("Change 4");
         println("YOutubeBranch")
         println("Thiss Line will be merged")
         // My first commit
