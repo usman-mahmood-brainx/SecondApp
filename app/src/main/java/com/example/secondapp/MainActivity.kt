3039f1767bd5c09a7242fd977bbd98dfb2b47722
usman.mahmood@brainxtech.com <bMGF!c3n>
2023-03-22 16:49:19 +0500

    Change 1 Commit

--- app/src/main/java/com/example/secondapp/MainActivity.kt
+++ app/src/main/java/com/example/secondapp/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
         var a = 2;
-        println("Experimental");
+        println("Change 1");
         println("YOutubeBranch")
         // My first commit
     }
